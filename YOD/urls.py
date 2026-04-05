d8b7c1a5e9ddbd8a4940b78e446ea1f685bf427a
Vasilii Kulak <mr.kulak.06@mail.ru>
2026-04-05 16:34:15 +0300

    fix4

--- YOD/urls.py
+++ YOD/urls.py
@@ -8,11 +8,11 @@ from django.views.static import serve
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('cart/', include('cart.urls', namespace='cart')),
     static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
