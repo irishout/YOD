295b8343245305af6ef4cec3c22d50d96ea991e6
Vasilii Kulak <mr.kulak.06@mail.ru>
2026-04-05 13:57:49 +0300

    for pythonanywhere

--- YOD/settings.py
+++ YOD/settings.py
@@ -29,9 +29,14 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False  
+
+ALLOWED_HOSTS = ['kulakv.pythonanywhere.com', 'localhost', '127.0.0.1'] 
+
+# Настройка статических файлов
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Важно для collectstatic [citation:3][citation:9]
 
 
 # Application definition
