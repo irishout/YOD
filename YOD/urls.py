8fec3e6b4aebb3acfa0b32712b9f038545075f71
Vasilii Kulak <mr.kulak.06@mail.ru>
2026-04-05 16:20:01 +0300

    mediafiles fix

--- YOD/urls.py
+++ YOD/urls.py
@@ -1,13 +1,15 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('cart/', include('cart.urls', namespace='cart')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
