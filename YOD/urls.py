ff6661b5ee8c75d81f5ba6d0e451414d4b292d30
Vasilii Kulak <mr.kulak.06@mail.ru>
2026-04-05 16:27:40 +0300

    mediafiles fix2

--- YOD/urls.py
+++ YOD/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('cart/', include('cart.urls', namespace='cart')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
